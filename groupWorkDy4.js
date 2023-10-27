03783e16c32a23e39c7707804b04f2d7a4ef4e9b
raam <jeya.colombo@gmail.com>
2023-10-27 10:17:31 -0400

    Created person class

--- groupWorkDy4.js
+++ groupWorkDy4.js
@@ -24,8 +24,68 @@ class Hamster {
     eatFood() {
         console.log('nibble nibble')
     }
-    eatFood() {
+    getPrice() {
         return this.price
     }
 }
 
+// Person
+// attributes:
+
+// name - set name from parameter in constructor method
+// age - initially 0
+// height - initially 0
+// weight - initially 0
+// mood - integer starting at 0 initially
+// hamsters - empty array initially
+// bankAccount - initially set to 0
+// methods:
+
+// getName() - returns name
+// getAge() - returns age
+// getWeight() - returns weight
+// greet() - logs a message with person's name
+// eat() - increment weight, increment mood
+// exercise() - decrement weight
+// ageUp() - increment age, increment height, increment weight, decrement mood, increment bank account by 10 (birthday money)
+// buyHamster(hamster) - push the hamster object onto the hamster array, increment mood by 10,
+// decrement bankAccount by the value of the hamster (hint: use getPrice())
+
+class Person {
+    constructor(name) {
+        this.name = name
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamsters = []
+        this.bankAccount = 0
+    }
+    getName() {
+        return this.name
+    }
+    getAge() {
+        return this.age
+    }
+    getWeight() {
+        return this.weight
+    }
+    greet() {
+        console.log(`I'm ${this.name}, Hello there`)
+    }
+    exercise() {
+        this.weight--
+    }
+    ageUp() {
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount += 10
+    }
+    buyHamster(hamster) {
+        this.hamsters.push(hamster)
+        this.mood += 10
+        this.bankAccount -= hamster.getPrice()
+    }
+}
